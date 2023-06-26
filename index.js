24865c9734e69befd4cdbba81bb2a5bc62129248
Aryan Suman <aryansuman18@gmail.com>
2023-06-27 00:53:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ app.use(cors())
 app.use(express.json());
 
 // routes
-app.use('/', require('./routes/root'))
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
