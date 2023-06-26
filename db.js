56d43fb38a1e5618dab222ec04008d2374275582
Aryan Suman <aryansuman18@gmail.com>
2023-06-26 23:41:34 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const mongoURI = "mongodb://localhost:27017/iNotebook";
 
 const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
+    mongoose.connect(process.env.MONGO_URI, ()=>{
         console.log("Connected");
     })
 }
